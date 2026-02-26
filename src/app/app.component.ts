c52c3fd3b833545a7022b420e27c51ea40f7a138
Pahola Ponsoy <pahola.ponsoy@hotmail.com>
2026-02-25 22:15:44 -0600

    agregue footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
 
 @Component({
+  standalone: true,
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'parcial1';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{ path: '', redirectTo: 'exchange', pathMatch: 'full' },
+	{
+		path: 'exchange',
+		loadComponent: () =>
+			import('./components/exchange/exchange.component').then((m) => m.ExchangeComponent),
+	},
+];
